a8f800d8cee6189967007afec9abe971009df307
rjgrunau <robertgrunau@gmail.com>
2019-05-03 21:39:24 -0700

    added photos

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,35 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from 'styled-components'
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import Img from "gatsby-image"
 import SEO from "../components/seo"
+
+const Container = styled.div`
+  width: 100%;
+  height: 700px;
+  background: linear-gradient(rgba(21,21,21,1),
+  rgba(21,21,21,0.3));
+  color: white;
+`
+
+const ImageHolder = styled.div`
+  width: 100%;
+  height: 400px;
+`
+const Image = styled(Img)`
+  height: 100%;
+  width: 100%;
+  background: cover;
+`
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    
+      <Container>
+        test
+      </Container>
   </Layout>
 )
 
