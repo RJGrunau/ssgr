350605fb70cbaacc771fff65edc6823c2f0d0dc8
rjgrunau <robertgrunau@gmail.com>
2019-05-05 15:21:21 -0700

    added background to small about to break up the blandness

--- src/components/smallAbout.js
+++ src/components/smallAbout.js
@@ -6,9 +6,12 @@ import Graphs from '../components/pargagraphs'
 const TextHolder = styled.div`
   width: 40%;
   margin-top: 100px;
+  padding: 15px;
   text-align: left;
-  backgournd: rgba(128,128,128,0.4);
+  background: rgba(128,128,128,0.3);
   text-align: left;
+  border-radius: 5px;
+  
 `
 const aboutText = `South Sound Guitar Repair is a guitar and bass repair shop in Tacoma, Washington specializing in vintage and high-end repair and set ups. 
 Repair and service are by appointment only. Get in  touch if you'd like to make an appointment to discuss your work on your instrument.`
