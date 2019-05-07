efa17e6ef08a3a77ea3b647d5a08f27e0e103363
rjgrunau <robertgrunau@gmail.com>
2019-05-07 08:04:15 -0700

    filled in contact page to make it work

---
+++ src/pages/contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components'
+
+
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+
+
+const ContactPage  = () => (
+    <Layout>
+        <SEO title="About" keywords={[`Jeff Hoppe`,`Tacoma Guitar Reair`]}/>
+    </Layout>
+)
+ 
+export default ContactPage ;
