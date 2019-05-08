62d54474d0eb7aa8719d759dfb69b1b44ee5d1fd
rjgrunau <robertgrunau@gmail.com>
2019-05-08 09:22:18 -0700

    added container for footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import styled from 'styled-components'
 
+import Graphs from '../components/pargagraphs'
+
+
 const SiteFooter = styled.footer`
     width: 100%;
-    background: grey;
+    background: rgba(128,128,128,0.2);
 `
+const FooterContainer = styled.div`
+    margin: 0 auto;
+    max-width: 1400px;
+    padding: 0px 1.0875rem 1.45rem;
+`
+
+
 const Footer = () => (
     <SiteFooter>
-        © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+        <FooterContainer>
+
+            © {new Date().getFullYear()}, Built with
+                {` `}
+                <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </FooterContainer>
     </SiteFooter>
 )
  
