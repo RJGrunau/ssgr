be2d953714fb0d6e5f2d2b035ed3a2a63cf402e8
rjgrunau <robertgrunau@gmail.com>
2019-05-09 08:22:56 -0700

    imported photos for gallery

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import styled from 'styled-components'
 
+import Layout from '../components/layout'
+import Container from '../components/container'
 
-const GalleryPage = () => {
-    return (  );
-}
+
+const GalleryPage = () => (
+    <Layout>
+      <Container>
+
+      </Container>
+    </Layout>
+)
+    
+
  
 export default GalleryPage;
