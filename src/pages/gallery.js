63791817cf086d7b4e94c1f50364a2bce3fa8551
rjgrunau <robertgrunau@gmail.com>
2019-05-09 08:50:39 -0700

    gallery image displays with static query

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,19 +1,56 @@
 import React from 'react';
-import { graphql } from 'gatsby'
+import { graphql, StaticQuery } from 'gatsby'
+import Img from 'gatsby-image'
 import styled from 'styled-components'
 
 import Layout from '../components/layout'
-import Container from '../components/container'
 
 
-const GalleryPage = () => (
+const GalleryContainer = styled.section`
+  width: 100%;
+  height: 100vh;
+  padding: 20px;
+  display: flex;
+  flex-flow: wrap row;
+`
+const GalleryImageHolder = styled.figure`
+  width: 33.3%;
+  height: 150px;
+`
+
+const GalleryImage = styled(Img)`
+
+`
+
+
+export default () => {
+  return(
+  <StaticQuery query={graphql`
+    {
+      doubleO18: file(relativePath: { eq: "0018.png"}) {
+        childImageSharp {
+          fluid(maxWidth: 400) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `}render={(data) => (
     <Layout>
-      <Container>
-
-      </Container>
+      <GalleryContainer>
+        <GalleryImageHolder
+          key={data.file}
+        >
+          <GalleryImage
+            fluid = {data.doubleO18.childImageSharp.fluid}
+          />
+        </GalleryImageHolder>
+      </GalleryContainer>
     </Layout>
-)
-    
+  )}
+  
+  />
+  )
+}
 
  
-export default GalleryPage;
