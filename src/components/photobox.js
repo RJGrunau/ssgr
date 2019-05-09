4b0f59ebde13b5b6353234e198a0bc3a5e0dfe5e
rjgrunau <robertgrunau@gmail.com>
2019-05-09 09:51:31 -0700

    extracted photo gallery images to a component for composability

---
+++ src/components/photobox.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Img from 'gatsby-image'
+import styled from 'styled-components'
+
+const GalleryImageHolder = styled.figure`
+  width: 25%;
+  height: 500px;
+  margin: 10px;
+  border-radius: 5px;
+  box-shadow: 3px 3px rgba(118,118,118,0.3);
+  z-index: 99;
+`
+
+const GalleryImage = styled(Img)`
+  width: 100%;
+  height: 100%;
+  background: cover;
+  border-radius: 5px;
+`
+const PhotoBox = (({image, alt}) => (
+    <GalleryImageHolder>
+        <GalleryImage
+            fluid = {image.childImageSharp.fluid}
+            alt = {alt}
+        />
+    </GalleryImageHolder>
+))
+ 
+export default PhotoBox;
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { graphql, StaticQuery } from 'gatsby'
-import Img from 'gatsby-image'
+
 import styled from 'styled-components'
 
 import Layout from '../components/layout'
-
+import PhotoBox from '../components/photobox'
 
 const GalleryContainer = styled.section`
   width: 100%;
@@ -14,54 +14,60 @@ const GalleryContainer = styled.section`
   display: flex;
   flex-flow: wrap row;
 `
-const GalleryImageHolder = styled.figure`
-  width: 25%;
-  height: 500px;
-  margin: 10px;
-  border-radius: 5px;
-  box-shadow: 3px 3px rgba(118,118,118,0.3);
-  z-index: 99;
-`
+// const GalleryImageHolder = styled.figure`
+//   width: 25%;
+//   height: 500px;
+//   margin: 10px;
+//   border-radius: 5px;
+//   box-shadow: 3px 3px rgba(118,118,118,0.3);
+//   z-index: 99;
+// `
 
-const GalleryImage = styled(Img)`
-  width: 100%;
-  height: 100%;
-  background: cover;
-  border-radius: 5px;
-`
+// const GalleryImage = styled(Img)`
+//   width: 100%;
+//   height: 100%;
+//   background: cover;
+//   border-radius: 5px;
+// `
 
 
 export default () => {
   return(
   <StaticQuery query={graphql`
     {
-      doubleO18: file(relativePath: { eq: "0018.png"}) {
+      doubleO18: file(relativePath: {eq: "0018.png"}){
         childImageSharp {
           fluid(maxWidth: 400) {
             ...GatsbyImageSharpFluid
           }
         }
       }
-    },
-    {
-      L00: file(relativePath: { eq: "L00.png"}) {
+      L00: file(relativePath: {eq: "L00.png"}) {
         childImageSharp {
           fluid(maxWidth: 400) {
             ...GatsbyImageSharpFluid
           }
         }
       }
-    }
+    } 
   `}render={(data) => (
     <Layout>
       <GalleryContainer>
-        <GalleryImageHolder
+        <PhotoBox
+          image = {data.doubleO18}
+          alt = "martin OO-18"
+        />
+        <PhotoBox
+          image = {data.L00}
+          alt = "Gibson L00"
+        />
+        {/* <GalleryImageHolder
           key={data.file}
         >
           <GalleryImage
             fluid = {data.doubleO18.childImageSharp.fluid}
           />
-        </GalleryImageHolder>
+        </GalleryImageHolder> */}
       </GalleryContainer>
     </Layout>
   )}
