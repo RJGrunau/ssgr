f1f5a7ff9ba29d7bda2c2710cbd2ee0be7536ea6
rjgrunau <robertgrunau@gmail.com>
2019-05-09 09:07:19 -0700

    added second photo

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -9,17 +9,25 @@ import Layout from '../components/layout'
 const GalleryContainer = styled.section`
   width: 100%;
   height: 100vh;
+  margin-top: 50px;
   padding: 20px;
   display: flex;
   flex-flow: wrap row;
 `
 const GalleryImageHolder = styled.figure`
-  width: 33.3%;
-  height: 150px;
+  width: 25%;
+  height: 500px;
+  margin: 10px;
+  border-radius: 5px;
+  box-shadow: 3px 3px rgba(118,118,118,0.3);
+  z-index: 99;
 `
 
 const GalleryImage = styled(Img)`
-
+  width: 100%;
+  height: 100%;
+  background: cover;
+  border-radius: 5px;
 `
 
 
@@ -28,6 +36,15 @@ export default () => {
   <StaticQuery query={graphql`
     {
       doubleO18: file(relativePath: { eq: "0018.png"}) {
+        childImageSharp {
+          fluid(maxWidth: 400) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    },
+    {
+      L00: file(relativePath: { eq: "L00.png"}) {
         childImageSharp {
           fluid(maxWidth: 400) {
             ...GatsbyImageSharpFluid
